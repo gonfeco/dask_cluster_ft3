00bd729b64f8ac664705fa6b0d8fe7e67ff0c8e1
gonfeco <gonfeco@gmail.com>
2022-11-30 09:02:16 +0100

    Changes to be committed: 	modified:   dask_cluster.py 	renamed:    dask_cluster_resvports.py -> dask_cluster_deprecated.py

--- dask_cluster.py
+++ dask_cluster.py
@@ -27,6 +27,18 @@ def nodeset_like(tira):
     list_of_nodes = [node_name + str(i) for i in list_of_nodes]
     return list_of_nodes
 
+def nodeset_like2(tira):
+    resto = tira
+    if '[' in resto:
+        resto = resto.replace('[', '')
+        resto = resto.replace(']', '')
+    nodes_ids = resto.split(',')
+    list_of_nodes = []
+    for i in nodes_ids:
+        list_of_nodes = list_of_nodes + nodeset_expand(i)
+    list_of_nodes = [str(i) for i in list_of_nodes]
+    return list_of_nodes
+
 def look_in_environment(environment_variable):
     """
     Esta funcion intentan evaluar la variable de entorno que se le pasa.
@@ -43,23 +55,35 @@ def look_in_environment(environment_variable):
         raise KeyError
     return value_variable
 
-def launch_scheduler():
-    dask_scheduler = 'dask-scheduler --port 0 --dashboard --interface ib0 --scheduler-file {}'.format(
-        './scheduler_info.json')
+def launch_scheduler(preload=None):
+    ports = look_in_environment('SLURM_STEP_RESV_PORTS')
+    port_list = nodeset_like2(ports)
+    local_id = int(look_in_environment("SLURM_LOCALID"))
+    print(port_list[local_id])
+    dask_scheduler = 'dask-scheduler --port {} --dashboard --interface ib0 --scheduler-file {}'.format(
+        port_list[local_id], './scheduler_info.json')
+    if preload is not None:
+        dask_scheduler = dask_scheduler + " --preload {}".format(preload)
     print('Command line to create Scheduler: {}'.format(dask_scheduler))
     #Lanzamos el comado que monta el Scheduler
     process = subprocess.run(dask_scheduler.split(), stdout=subprocess.PIPE)
     return process
 
-def launch_worker(scheduler_file=None, scheduler_address=None, local_folder='/tmp'):
+def launch_worker(scheduler_file=None, scheduler_address=None, local_folder='/tmp', preload=None):
+    ports = look_in_environment('SLURM_STEP_RESV_PORTS')
+    port_list = nodeset_like2(ports)
+    local_id = int(look_in_environment("SLURM_LOCALID"))
+    print(port_list[local_id])
     if scheduler_file is not None:
-        worker = 'dask-worker --scheduler-file {} --interface ib0 --no-nanny --nthreads 1 --local-directory {}'.format(
-            scheduler_file, local_folder)
+        worker = 'dask-worker --scheduler-file {} --worker-port {} --interface ib0 --no-nanny --nthreads 1 --local-directory {}'.format(
+            scheduler_file, port_list[local_id], local_folder)
     elif scheduler_address is not None:
-        worker = 'dask-worker {} --interface ib0 --no-nanny --nthreads 1 --local-directory {}'.format(
-            scheduler_address, local_folder)
+        worker = 'dask-worker {} --worker-port {} --interface ib0 --no-nanny --nthreads 1 --local-directory {}'.format(
+            scheduler_address, port_list[local_id], local_folder)
     else:
         raise ValueError('No tengo direccion del scheduler')
+    if preload is not None:
+        worker = worker + " --preload {}".format(preload)
     print('Command line to create worker: {}'.format(worker))
     #Lanzamos el comando que levanta el Worker
     process = subprocess.run(worker.split(), stdout=subprocess.PIPE)
@@ -108,10 +132,12 @@ def create_ssh_file(json_file_name):
     print(tira_ssh)
     return tira_ssh
 
-def create_dask_client(json_file_name="./scheduler_info.json"):
+def create_dask_client(json_file_name):
     #Test if json scheduler exist
     from distributed import Client
     json_file_name = test_scheduler_file(json_file_name)
+    print(json_file_name)
+    #json_file_name = os.stat(json_file_name)
     dask_client = Client(scheduler_file=json_file_name)
     return dask_client
 
@@ -150,7 +176,7 @@ if __name__ == "__main__":
     parser.add_argument(
         "-scheduler_address",
         dest="scheduler_address",
-        type=int,
+        type=str,
         default=None,
         help="Scheduler IP. ex: tcp://10.120.10.7:8085",
     )
@@ -161,15 +187,24 @@ if __name__ == "__main__":
         action="store_true",
         help="Get the dask client",
     )
+    parser.add_argument(
+        '-preload',
+        dest="preload",
+        default=None,
+        help="File for preload code in dask scheduler and workers."
+    )
     FLAGS, unparsed = parser.parse_known_args()
 
     if FLAGS.scheduler:
-        scheduler_dask = launch_scheduler()
+        scheduler_dask = launch_scheduler(
+            preload=FLAGS.preload
+        )
     if FLAGS.worker:
         if FLAGS.scheduler_address is not None:
             worker_dask = launch_worker(
                 scheduler_address=FLAGS.scheduler_address,
-                local_folder=FLAGS.local
+                local_folder=FLAGS.local,
+                preload=FLAGS.preload
             )
         else:
             #Verifica que el json de configuracion existe
@@ -177,7 +212,7 @@ if __name__ == "__main__":
             worker_dask = launch_worker(
                 scheduler_file='./scheduler_info.json',
                 scheduler_address=FLAGS.scheduler_address,
-                local_folder=FLAGS.local
+                local_folder=FLAGS.local, preload=FLAGS.preload
             )
 
     if FLAGS.ssh_file:
@@ -187,7 +222,7 @@ if __name__ == "__main__":
         f.close()
 
     if FLAGS.client:
-        dask_client = create_dask_client("./scheduler_info.json")
+        dask_client = create_dask_client('./scheduler_info.json')
         print(dask_client)
 
     if FLAGS.dask_cluster:
@@ -199,7 +234,9 @@ if __name__ == "__main__":
         final_id = int(look_in_environment('SLURM_PROCID'))
 
         if final_id == 0:
-            scheduler_dask = launch_scheduler()
+            scheduler_dask = launch_scheduler(
+                preload=FLAGS.preload
+            )
         else:
             #Todos las tareas worker comprueban que existe el fichero
             #de configuracion del cluster
@@ -213,5 +250,6 @@ if __name__ == "__main__":
             worker_dask = launch_worker(
                 scheduler_file='./scheduler_info.json',
                 scheduler_address=None,
-                local_folder=FLAGS.local
+                local_folder=FLAGS.local,
+                preload=FLAGS.preload
             )
--- dask_cluster_deprecated.py
+++ dask_cluster_deprecated.py
@@ -27,18 +27,6 @@ def nodeset_like(tira):
     list_of_nodes = [node_name + str(i) for i in list_of_nodes]
     return list_of_nodes
 
-def nodeset_like2(tira):
-    resto = tira
-    if '[' in resto:
-        resto = resto.replace('[', '')
-        resto = resto.replace(']', '')
-    nodes_ids = resto.split(',')
-    list_of_nodes = []
-    for i in nodes_ids:
-        list_of_nodes = list_of_nodes + nodeset_expand(i)
-    list_of_nodes = [str(i) for i in list_of_nodes]
-    return list_of_nodes
-
 def look_in_environment(environment_variable):
     """
     Esta funcion intentan evaluar la variable de entorno que se le pasa.
@@ -55,31 +43,27 @@ def look_in_environment(environment_variable):
         raise KeyError
     return value_variable
 
-def launch_scheduler():
-    ports = look_in_environment('SLURM_STEP_RESV_PORTS')
-    port_list = nodeset_like2(ports)
-    local_id = int(look_in_environment("SLURM_LOCALID"))
-    print(port_list[local_id])
-    dask_scheduler = 'dask-scheduler --port {} --dashboard --interface ib0 --scheduler-file {}'.format(
-        port_list[local_id], './scheduler_info.json')
+def launch_scheduler(preload=None):
+    dask_scheduler = 'dask-scheduler --port 0 --dashboard --interface ib0 --scheduler-file {}'.format(
+        './scheduler_info.json')
+    if preload is not None:
+        dask_scheduler = dask_scheduler + " --preload {}".format(preload)
     print('Command line to create Scheduler: {}'.format(dask_scheduler))
     #Lanzamos el comado que monta el Scheduler
-    process = subprocess.run(dask_scheduler.split(), stdout=subprocess.PIPE)
-    return process
+    #process = subprocess.run(dask_scheduler.split(), stdout=subprocess.PIPE)
+    #return process
 
-def launch_worker(scheduler_file=None, scheduler_address=None, local_folder='/tmp'):
-    ports = look_in_environment('SLURM_STEP_RESV_PORTS')
-    port_list = nodeset_like2(ports)
-    local_id = int(look_in_environment("SLURM_LOCALID"))
-    print(port_list[local_id])
+def launch_worker(scheduler_file=None, scheduler_address=None, local_folder='/tmp', preload=None):
     if scheduler_file is not None:
-        worker = 'dask-worker --scheduler-file {} --worker-port {} --interface ib0 --no-nanny --nthreads 1 --local-directory {}'.format(
-            scheduler_file, port_list[local_id], local_folder)
+        worker = 'dask-worker --scheduler-file {} --interface ib0 --no-nanny --nthreads 1 --local-directory {}'.format(
+            scheduler_file, local_folder)
     elif scheduler_address is not None:
-        worker = 'dask-worker {} --worker-port {} --interface ib0 --no-nanny --nthreads 1 --local-directory {}'.format(
-            scheduler_address, port_list[local_id], local_folder)
+        worker = 'dask-worker {} --interface ib0 --no-nanny --nthreads 1 --local-directory {}'.format(
+            scheduler_address, local_folder)
     else:
         raise ValueError('No tengo direccion del scheduler')
+    if preload is not None:
+        worker = worker + " --preload {}".format(preload)
     print('Command line to create worker: {}'.format(worker))
     #Lanzamos el comando que levanta el Worker
     process = subprocess.run(worker.split(), stdout=subprocess.PIPE)
@@ -128,12 +112,10 @@ def create_ssh_file(json_file_name):
     print(tira_ssh)
     return tira_ssh
 
-def create_dask_client(json_file_name):
+def create_dask_client(json_file_name="./scheduler_info.json"):
     #Test if json scheduler exist
     from distributed import Client
     json_file_name = test_scheduler_file(json_file_name)
-    print(json_file_name)
-    #json_file_name = os.stat(json_file_name)
     dask_client = Client(scheduler_file=json_file_name)
     return dask_client
 
@@ -172,7 +154,7 @@ if __name__ == "__main__":
     parser.add_argument(
         "-scheduler_address",
         dest="scheduler_address",
-        type=str,
+        type=int,
         default=None,
         help="Scheduler IP. ex: tcp://10.120.10.7:8085",
     )
@@ -183,15 +165,25 @@ if __name__ == "__main__":
         action="store_true",
         help="Get the dask client",
     )
+    parser.add_argument(
+        '-preload',
+        dest="preload",
+        default=None,
+        help="File for preload code in dask scheduler and workers."
+    )
     FLAGS, unparsed = parser.parse_known_args()
+    print(FLAGS)
 
     if FLAGS.scheduler:
-        scheduler_dask = launch_scheduler()
+        scheduler_dask = launch_scheduler(
+            preload=FLAGS.preload
+        )
     if FLAGS.worker:
         if FLAGS.scheduler_address is not None:
             worker_dask = launch_worker(
                 scheduler_address=FLAGS.scheduler_address,
-                local_folder=FLAGS.local
+                local_folder=FLAGS.local,
+                preload=FLAGS.preload
             )
         else:
             #Verifica que el json de configuracion existe
@@ -199,7 +191,8 @@ if __name__ == "__main__":
             worker_dask = launch_worker(
                 scheduler_file='./scheduler_info.json',
                 scheduler_address=FLAGS.scheduler_address,
-                local_folder=FLAGS.local
+                local_folder=FLAGS.local,
+                preload=FLAGS.preload
             )
 
     if FLAGS.ssh_file:
@@ -209,7 +202,7 @@ if __name__ == "__main__":
         f.close()
 
     if FLAGS.client:
-        dask_client = create_dask_client('./scheduler_info.json')
+        dask_client = create_dask_client("./scheduler_info.json")
         print(dask_client)
 
     if FLAGS.dask_cluster:
@@ -221,7 +214,9 @@ if __name__ == "__main__":
         final_id = int(look_in_environment('SLURM_PROCID'))
 
         if final_id == 0:
-            scheduler_dask = launch_scheduler()
+            scheduler_dask = launch_scheduler(
+                preload=FLAGS.preload
+            )
         else:
             #Todos las tareas worker comprueban que existe el fichero
             #de configuracion del cluster
@@ -235,5 +230,6 @@ if __name__ == "__main__":
             worker_dask = launch_worker(
                 scheduler_file='./scheduler_info.json',
                 scheduler_address=None,
-                local_folder=FLAGS.local
+                local_folder=FLAGS.local,
+                preload=FLAGS.preload
             )
